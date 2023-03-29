57a67f4e159e5c7d403a77f477b1152d8ce4845d
jycouet <jycouet@gmail.com>
2023-03-29 19:54:28 +0200

    update titles & images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	<title>Houdini • TodoMVC</title>
+	<title>Houdini • SvelteKit • TodoMVC</title>
 	<link rel="stylesheet" href="//unpkg.com/todomvc-common/base.css" />
 	<link rel="stylesheet" href="//unpkg.com/todomvc-app-css/index.css" />
 </svelte:head>
@@ -10,6 +10,9 @@
 
 <footer class="info">
 	<p>Double-click to edit a todo</p>
-	<p>Created by <a href="http://todomvc.com">Alec Aivazis</a></p>
+	<p>
+		Created by <a href="https://github.com/AlecAivazis">Alec Aivazis</a> with
+		<a href="https://github.com/HoudiniGraphql/houdini">Houdini</a>
+	</p>
 	<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
 </footer>
